8c6ce41f7d5d45c31e9791477546449081f6f807
rochafialvin <rochafi.dev@gmail.com>
2019-08-13 10:42:47 +0700

    Backhandler

--- src/app/DiaryScreen.js
+++ src/app/DiaryScreen.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react'
-import {View, Text} from 'react-native'
+import {View, Text, BackHandler} from 'react-native'
+import {NavigationEvents} from 'react-navigation'
 
 class DiaryScreen extends Component {
+
+    onBackButton = () => {
+        alert('Tombol back di tekan')
+        // Menonaktifkan default function (kembali ke halaman sebelumnya)
+        return true
+    }
+
     render() {
         return (
             <View>
+                <NavigationEvents
+                    // ComponentDidMount
+                    onDidFocus = {() => {
+                        BackHandler.addEventListener('hardwareBackPress', this.onBackButton)
+                    }}
+
+                    // ComponentWillUnmount
+                    onWillBlur = {() => {
+                        BackHandler.removeEventListener('hardwareBackPress', this.onBackButton)
+                    }}
+                />
                 <Text>DiaryScreen</Text>
             </View>
         )
-    }
+    
 }
 
 export default DiaryScreen
