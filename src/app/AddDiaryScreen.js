ab560a24b980cda739bf3af19741a50b4ed050c3
rochafialvin <rochafi.dev@gmail.com>
2019-08-14 10:09:17 +0700

    Add Date Picker

--- src/app/AddDiaryScreen.js
+++ src/app/AddDiaryScreen.js
@@ -1,29 +1,55 @@
 import React, { Component } from 'react'
-import {View} from 'react-native'
+import {View, StyleSheet} from 'react-native'
 import {Container, Text, Textarea, Button, Item, Input} from 'native-base'
 
+import DatePick from './components/DatePick'
+
 class AddDiaryScreen extends Component {
+    getDate = () => {
+        alert('Function dari AddDiaryScreen')
+    }
+
     render() {
         return (
             <Container>
-                <Item rounded>
-                    <Input
-                        placeholder='Title'
-                    />
-                </Item>
-                <Textarea
-                    placeholder = 'Your Story'
-                    bordered
-                    rowSpan = {15}
-                />
-                <View>
-                    <Button>
-                        <Text>Create Diary</Text>
-                    </Button>
+                <View style={styles.container}>
+                    <Text style={{fontSize: 20}}>Create Diary</Text>
+                    <View style={styles.wrapper}>
+                        <DatePick funDate={this.getDate}/>
+                        <Item rounded>
+                            <Input
+                                placeholder='Title'
+                            />
+                        </Item>
+                        <Textarea
+                            placeholder = 'Your Story'
+                            bordered
+                            rowSpan = {15}
+                        />
+                        <View style={styles.button}>
+                            <Button block>
+                                <Text>Create Diary</Text>
+                            </Button>
+                        </View>
+                    </View>
                 </View>
             </Container>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center'
+    },
+    wrapper: {
+        width: '90%',
+        marginTop: 15
+    },
+    button: {
+        marginTop: 10
+    }
+})
 
 export default AddDiaryScreen
